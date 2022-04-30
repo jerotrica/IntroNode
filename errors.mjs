173c022c8e96d188771daaadbb6b87722ef4c67a
Jeronimo <jerotrica@gmail.comm>
2022-04-30 17:39:44 -0300

    Error Handling: UnCaughtException

--- errors.mjs
+++ errors.mjs
@@ -1,7 +1,10 @@
+// Title:  unCaught Exception
+// Description: Just for exceptions, for finding a quick solution fix, knowing what the problem is
+
 import { readFile } from 'fs/promises'
 
-
-const result = readFile(new URL('app.mj s', import.meta.url), 'utf-8')
-    .catch(e => {
-        console.log("Herrro")
-    })
+process.on('uncaughtException', (e) => {
+    console.log(e)
+})
+const result = await readFile(new URL('app.mj s', import.meta.url), 'utf-8')
+console.log("hello")
