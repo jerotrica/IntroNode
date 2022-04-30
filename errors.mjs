b5fb72dc384e9b0173a2a87926529d74ead19567
Jeronimo <jerotrica@gmail.comm>
2022-04-30 17:32:52 -0300

    Handling errors

--- errors.mjs
+++ errors.mjs
@@ -1,7 +1,7 @@
 import { readFile } from 'fs/promises'
 
 
-const result = await readFile(new URL('app.mjs', import.meta.url), 'utf-8')
+const result = readFile(new URL('app.mj s', import.meta.url), 'utf-8')
     .catch(e => {
         console.log("Herrro")
     })
